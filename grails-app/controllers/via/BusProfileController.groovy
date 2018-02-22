5eb47fe31ec98427b98a218d265860f609817916
Vivian <vcnales@gmail.com>
2018-02-22 00:39:32 -0600

    BusController added

---
+++ grails-app/controllers/via/BusProfileController.groovy
@@ -0,0 +1,10 @@
+package via
+
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_ADMIN', 'ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+
+class BusProfileController {
+
+    def index() { }
+}
