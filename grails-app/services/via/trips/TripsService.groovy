0929d163b7796a858cdf129d966a097e8a371222
Vivian <vcnales@gmail.com>
2018-02-24 20:36:01 -0600

    Added functionality to save trip to db & to get all user routes

--- grails-app/services/via/trips/TripsService.groovy
+++ grails-app/services/via/trips/TripsService.groovy
@@ -4,7 +4,10 @@ import com.via.auth.User
 import grails.transaction.Transactional
 import via.Route
 import via.ServiceResponse
+import via.Trip
 import via.UserProfile
+
+import javax.xml.ws.Service
 
 @Transactional
 class TripsService {
@@ -20,6 +23,7 @@ class TripsService {
             route.destination = params.endingAddress
             route.distanceInMiles = params.distanceInMiles
             route.routeName = params.tripName
+            route.durationInMinutes = params.durationInMinutes
             route.userProfile = userProfile
 
             Route persistedRoute =  route.save(flush: true, failOnError: true)
@@ -36,5 +40,68 @@ class TripsService {
             serviceResponse.fail("Error saving trip information.")
         }
         return serviceResponse
+    }
+
+
+    def getUserRoutes(){
+        ServiceResponse serviceResponse = new ServiceResponse()
+
+        try{
+            UserProfile userProfile = userService.getLoggedInUserProfile()
+            log.info(userProfile)
+
+            def routeList = Route.findAllByUserProfile(userProfile)
+
+            if(routeList.size() > 0){
+                serviceResponse.success = true
+                serviceResponse.result = routeList
+
+            } else{
+                serviceResponse.success = false
+                serviceResponse.fail("Failed to save route")
+
+            }
+
+        }catch(Exception e){
+            log.error("Error retrieving user routes: " + e)
+            serviceResponse.fail("Error retrieving user routes.")
+        }
+        return serviceResponse
+
+    }
+
+    def logTripToDatabase(params){
+        ServiceResponse serviceResponse = new ServiceResponse()
+
+        try{
+            UserProfile userProfile = userService.getLoggedInUserProfile()
+            Route route = Route.findById(params.routeId)
+            log.info(userProfile)
+            log.info(route)
+
+            Date date = new Date(params.tripDate)
+
+            Trip trip = new Trip()
+            trip.dateOfTrip = date
+            trip.userProfile = userProfile
+            trip.route = route
+
+            Trip persistedTrip =  trip.save(flush: true, failOnError: true)
+            if(persistedTrip){
+                serviceResponse.success = true
+                serviceResponse.result = persistedTrip
+            }else{
+                log.info(params)
+                log.info(trip)
+                serviceResponse.success = false
+                serviceResponse.fail("Failed to save route")
+            }
+
+        }catch(Exception e){
+            log.error("Error logging trip: " + e)
+            serviceResponse.fail("Error loggint trip.")
+        }
+        return serviceResponse
+
     }
 }
